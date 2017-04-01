d05b9cee79427d1fb23e63de40ff30472c796f02
hirotaka <musicfair@gmail.com>
2017-04-01 10:00:06 +0900

    First Commit

---
+++ app/src/main/java/com/example/hirotaka/asampleintentcamera/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.hirotaka.asampleintentcamera;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent();
+                    intent.setAction( MediaStore.ACTION_IMAGE_CAPTURE );
+                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+
+                    startActivityForResult(intent, 0);
+                }
+                catch (Exception e) {
+                    Log.d( "MoonTracker", "Intent MediaStore.ACTION_IMAGE_CAPTURE Error" );
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String string;
+
+        string = "requestCode " + String.valueOf(requestCode) + " " +
+                 "resultCode " + String.valueOf(resultCode);
+        Toast.makeText( this, string, Toast.LENGTH_LONG).show();
+    }
+}
